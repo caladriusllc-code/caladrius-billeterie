8b0a49b113901b1ed25a8e78edc138a71dc46d27
miakojean <miakojeanyves@gmail.com>
2026-05-17 00:21:01 +0000

    work on design implementing

---
+++ billeteriefront/app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-container">
+        <div class="pic-container">
+            
+        </div>
+        <div class="info-container">
+
+        </div>
+
+        <div class="description-container">
+            <p>
+                Bienvenu au pays mon fils
+            </p>
+        </div>
+    </div>
+</template>
