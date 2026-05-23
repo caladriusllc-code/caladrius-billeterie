93d265e4a7dc1601162481718376c9a936d5bb53
miakojean <miakojeanyves@gmail.com>
2026-05-23 18:57:17 +0000

    work authentication

---
+++ billeteriefront/app/components/forms/loginForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-container">
+        <h2>Login Form</h2>
+    </div>
+</template>
+
+<script>
+import BaseInputVue from '../input/BaseInput.vue'
+
+export default {
+    components:{
+        BaseInputVue
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ billeteriefront/app/pages/auth/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-wrapper">
+        <h1>Bienvenenu au pays mon filis</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
